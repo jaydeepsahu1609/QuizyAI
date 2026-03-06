6820339433aa3ad47047f9ddf22c7032beb89001
jaydeepsahu1609 <sahujaydeep4321@gmail.com>
2026-03-06 19:12:43 +0530

    add springboot project
    extracted zip file generated by Spring Initializr

---
+++ src/main/java/com/jaydeep/quizyai/QuizyaiApplication.java
@@ -0,0 +1,13 @@
+package com.jaydeep.quizyai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizyaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizyaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaydeep/quizyai/QuizyaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaydeep.quizyai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizyaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
